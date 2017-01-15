2d7c76466a5ceb3b266a2e6526d54eba649e1dca
firezenk <firezenk@gmail.com>
2017-01-15 13:24:18 +0100

    Add a recycler view to the activity and set its layout manager

--- app/src/main/java/com/firezenk/ksp/MainActivity.kt
+++ app/src/main/java/com/firezenk/ksp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.firezenk.ksp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
