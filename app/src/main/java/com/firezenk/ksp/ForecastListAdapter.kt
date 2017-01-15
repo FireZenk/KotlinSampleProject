0b2b2380efdd14b2ae3478d8460907cae8723de3
firezenk <firezenk@gmail.com>
2017-01-15 16:11:25 +0100

    Use the new command request to populate the data into the list

--- app/src/main/java/com/firezenk/ksp/ForecastListAdapter.kt
+++ app/src/main/java/com/firezenk/ksp/ForecastListAdapter.kt
@@ -3,18 +3,21 @@ package com.firezenk.ksp
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import com.firezenk.ksp.domain.ForecastList
 
-class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(TextView(parent.context))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        with(weekForecast.dailyForecastList[position]) {
+            holder.textView.text = "$date - $description $high/$low"
+        }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForecast.dailyForecastList.size
 
     class ViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
 }
--- app/src/main/java/com/firezenk/ksp/MainActivity.kt
+++ app/src/main/java/com/firezenk/ksp/MainActivity.kt
@@ -4,26 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
-import com.firezenk.ksp.data.ForecastRequest
+import com.firezenk.ksp.domain.RequestForecastCommand
 import org.jetbrains.anko.async
 
 import org.jetbrains.anko.find
-import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thu 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,23 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         val forecastList : RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
-
-        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
-                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
 
         async() {
-            ForecastRequest(url).execute()
+            val result = RequestForecastCommand("94043").execute()
             uiThread {
-                toast("ForecastRequest performed")
+                forecastList.adapter = ForecastListAdapter(result)
             }
         }
-
-        val f1 = Forecast(Date(), 27.5f, "Shiny day")
-        val f2 = f1.copy(temperature = 30f)
-
-        val (date, temperature, details) = f1
-
-        Log.d(javaClass.simpleName, "$date $temperature $details")
     }
 }
