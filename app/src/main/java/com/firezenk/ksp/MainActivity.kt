ccdc78ac1751517865aed626f2ffdf461e397528
firezenk <firezenk@gmail.com>
2017-01-15 13:50:31 +0100

    Make a sample request and show a result

--- app/src/main/java/com/firezenk/ksp/MainActivity.kt
+++ app/src/main/java/com/firezenk/ksp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.async
 
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +28,12 @@ class MainActivity : AppCompatActivity() {
         val forecastList : RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        async() {
+            Request("https://github.com/FireZenk/KotlinSampleProject").run()
+            uiThread {
+                toast("Request performed")
+            }
+        }
     }
 }
